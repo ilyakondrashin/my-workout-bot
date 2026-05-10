7943b39421b7a0b8802f57024bf65abae9e7953b
ilyakondrashin <ilya.kondrashin.al@gmail.com>
2026-05-10 21:58:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,5 +20,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    print("🤖 Бот запущен!")
-    app.run_polling(drop_pending_upd
+    print("Бот запущен!")
+    app.run_polling(drop_pending_updates=True)
+
+if __name__ == "__main__":
+    main()
